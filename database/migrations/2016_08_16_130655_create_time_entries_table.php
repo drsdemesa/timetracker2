992eca154a0c075e13cd38c3469fb99f754e884b
drsdemesa <macbookpro@MacbookPros-MacBook-Pro.local>
2016-08-16 21:11:27 +0800

    with migrated data - users, time_entries

--- database/migrations/2016_08_16_130655_create_time_entries_table.php
+++ database/migrations/2016_08_16_130655_create_time_entries_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTimeEntriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('time_entries', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id')->unsigned();
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->string('comment')->nullable();
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('time_entries');
     }
 }
